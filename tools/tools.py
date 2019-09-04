8519a08831b1a4b6020f9bffe1f212317654ce96
Stefan <39183620+Areskiko@users.noreply.github.com>
2019-09-04 10:54:55 +0200

    Added  descriptions

--- tools/tools.py
+++ tools/tools.py
@@ -5,22 +5,27 @@ import time
 import sys
 
 def construct(expression, var="x"):
+    """Constructs a function, assign function return to a variable"""
     def f(x):
         return eval(expression.replace(var, "x"))
     return f
 
 class loading():
+    """Loading class"""
     def start(self, flavor="loading"):
+        """Starts a loading sequence"""
         self.done=False
         self.flavor = flavor
         t = threading.Thread(target=self.animate, daemon=True)
         t.start()
 
     def stop(self):
+        "Stops a loading sequence"
         self.done=True
         time.sleep(0.2)
 
     def animate(self):
+        "DO NOT USE, internal function"
         for c in itertools.cycle(['|', '/', '-', '\\']):
             if self.done:
                 break
