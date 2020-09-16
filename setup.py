90f756e44ea555dea901c95a13c8a408ff62f377
Stefan <39183620+Areskiko@users.noreply.github.com>
2020-09-16 17:41:02 +0200

    Added dependency requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pmxutils",
-    version="0.0.8",
+    version="0.1.0",
     author="Stefan Mack",
     author_email="stefan_mack@hotmail.com",
     description="Collection of tools for programmation and modeling X",
@@ -19,4 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+    ],
 )
